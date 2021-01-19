a5a871ac72b28f28fdd714e420894585ed4443dd
Ricardo Ramirez <r1ck2rdo@hotmail.com>
2021-01-19 00:34:58 -0300

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,34 @@ export class InfoPaginaService {
   
   info: InfoPagina ={};
   cargada=false;
+  equipo: any[]=[];
 
   constructor(private http: HttpClient) { 
 
     console.log('Servicio de infoPagina listo');
 
-    //Leer archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp:InfoPagina) =>{
-        //console.log(resp);
         this.cargada=true;
         this.info=resp;
         console.log( resp );
       })
   }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-916ca-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp:any) =>{
+        this.equipo=resp;
+        console.log( resp );
+      })
+
+  }
+
+
+
 }
